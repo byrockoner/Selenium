38fe2c03158270183fd14f0ca3b6bc0f59718136
byrockoner <bayrakoner@gmail.com>
2023-03-29 02:57:58 +0300

    ilk web sayfası acıldı

--- src/Gun01.java
+++ src/Gun01.java
@@ -1,5 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
 public class Gun01 {
     public static void main(String[] args) {
-        System.out.println("Selenium Eğitim Projesi");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.techno.study/");
+
+        //  WebDriver driver1 =new EdgeDriver();
+        //  driver1.get("https://www.techno.study/");
+        //  WebDriver driver2 = new SafariDriver();
+        //  driver2.get("https://www.techno.study/");
+
+
     }
 }
